aeacb25ec72cdd76a56635e3478e3a4e0f9ba031
Julio Zampier <juliozampier@gmail.com>
2023-01-06 01:02:49 -0300

    Add 01 - 04 sql files

---
+++ 02-create-users-table.sql
@@ -0,0 +1,8 @@
+--!PostgreSQL -> We have to create a custom type first
+CREATE TYPE employment_status AS ENUM('employed', 'unemployed', 'self-employed');
+CREATE TABLE IF NOT EXISTS users(
+    full_name VARCHAR(200),
+    yearly_salary INTEGER,
+    current_status employment_status
+    --current_status ENUM('employed', 'unemployed', 'self-employed') --? MySQL
+);
---
+++ 03-insert-users-data.sql
@@ -0,0 +1,3 @@
+INSERT INTO users (yearly_salary, full_name, current_status)
+VALUES (45000,'Pedro Sampaio', 'employed'),
+(0,'Roberval Chocolate Stealer', 'unemployed');
