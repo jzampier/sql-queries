e20670b132a53cdc9b638b5341faed46a341707b
Julio Zampier <juliozampier@gmail.com>
2023-01-09 20:15:02 -0300

    Insert some data on conversation and employers tables

--- 07-insert-data.sql
+++ 07-insert-data.sql
@@ -1,4 +1,4 @@
--- Active: 1673067291495@@127.0.0.1@3306@talently
+-- Active: 1673067490537@@127.0.0.1@5432@talently
 INSERT INTO employers (company_name, company_adress, yearly_revenue, is_hiring)
 VALUES
 ('PETROBRAS', 'Elias Agostinho 665 Rio de Janeiro', 200, true),
@@ -7,7 +7,7 @@ VALUES
 ('Power of Minas Inc', 'Center of the World 2022, Belzonte', 543, true);
 
 INSERT INTO conversation (user_name, employer_name, message, date_sent) VALUES
-('Pedro Sampaio', '', '', ''),
-('Roberval Chocolate Setealer', '', '', ''),
-('', '', '', ''),
-('', '', '', ''),
+('Pedro Sampaio', 'PETROBRAS', 'Text message 1 blablabla', '2022-01-06'),
+('Roberval Chocolate Setealer', 'Salumba Inc', 'Text message 2 hueueueue', '2022-01-06'),
+('Pedro Sampaio', 'CFM Ltda', 'Gibe mony plox', '2022-11-06'),
+('Pedro Sampaio', 'Power of Minas Inc', 'Text Text Text Text Text Text Text ', '2023-01-09');
