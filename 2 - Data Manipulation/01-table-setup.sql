6d4182052ee2bfb0a382ab6a68dc2e1ecc1374cc
Julio Zampier <juliozampier@gmail.com>
2023-01-12 00:15:20 -0300

    insert data on sales table

--- 2 - Data Manipulation/01-table-setup.sql
+++ 2 - Data Manipulation/01-table-setup.sql
@@ -1,2 +1,26 @@
--- Active: 1673125002031@@127.0.0.1@5432@online_shop@public
+-- Active: 1673125002031@@127.0.0.1@5432@sales_example@public
 CREATE DATABASE sales_example;
+-- !Postgres
+CREATE TABLE sales (
+    id SERIAL PRIMARY KEY,
+    date_created DATE DEFAULT CURRENT_DATE,
+    date_fulfilled DATE,
+    customer_name VARCHAR(300) NOT NULL,
+    product_name VARCHAR(300) NOT NULL,
+    volume NUMERIC(10,3) NOT NULL CHECK (volume >=0),
+    is_recurring BOOLEAN DEFAULT FALSE,
+    is_disputed BOOLEAN DEFAULT FALSE
+);
+
+
+-- ?MySQL
+CREATE TABLE sales (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    date_created DATE DEFAULT (CURRENT_DATE),
+    date_fulfilled DATE,
+    customer_name VARCHAR(300) NOT NULL,
+    product_name VARCHAR(300) NOT NULL,
+    volume NUMERIC(10,3) NOT NULL CHECK (volume >=0),
+    is_recurring BOOLEAN DEFAULT FALSE,
+    is_disputed BOOLEAN DEFAULT FALSE
+);
---
+++ 2 - Data Manipulation/02-insert-data.sql
@@ -0,0 +1,37 @@
+-- Active: 1673124617232@@127.0.0.1@3306@sales_example
+-- INSERT INTO sales (
+--     customer_name,
+--     product_name,
+--     volume,
+--     is_recurring
+--     )
+-- VALUES(
+--     'Max Schwarz',
+--     'A Book',
+--     12.99,
+--     TRUE
+-- );
+
+INSERT INTO sales (
+    date_fulfilled,
+    customer_name,
+    product_name,
+    volume,
+    is_recurring,
+    is_disputed
+    )
+VALUES(
+    NULL,
+    'Learing Inc',
+    'Course Bundle',
+    '4889.62',
+    FALSE,
+    FALSE
+), (
+    '2022-04-10',
+    'Big Oil Inc',
+    'Trucks',
+    400000.0,
+    FALSE,
+    TRUE
+);
