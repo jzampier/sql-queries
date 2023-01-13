a34e0c41ed40104ecbea3df2ff5764d08fea3c6c
Julio Zampier <juliozampier@gmail.com>
2023-01-12 22:27:55 -0300

    Tested formatter

--- 2 - Data Manipulation/13-distinct-values.sql
+++ 2 - Data Manipulation/13-distinct-values.sql
@@ -1,4 +1,12 @@
-SELECT DISTINCT customer_name FROM sales;
+-- Active: 1673125002031@@127.0.0.1@5432@sales_example
+SELECT
+    DISTINCT customer_name
+FROM
+    sales;
 
-SELECT DISTINCT customer_name FROM sales
-ORDER BY customer_name;
+SELECT
+    DISTINCT customer_name
+FROM
+    sales
+ORDER BY
+    customer_name;
--- 2 - Data Manipulation/14-subqueries.sql
+++ 2 - Data Manipulation/14-subqueries.sql
@@ -1,14 +1,33 @@
 -- Active: 1673125002031@@127.0.0.1@5432@sales_example
 -- !SELECT <columns> FROM (<subquery>) AS <subquery_name>;
-SELECT customer_name, product_name
-FROM (SELECT * FROM sales
-WHERE volume > 1000) AS base_result;
+SELECT
+    customer_name,
+    product_name
+FROM
+    (
+        SELECT
+            *
+        FROM
+            sales
+        WHERE
+            volume > 1000
+    ) AS base_result;
 
 -- ?Creating a view and storing it in base_result
-CREATE VIEW base_result AS SELECT * FROM sales
-WHERE volume > 1000;
-SELECT customer_name, product_name
-FROM base_result;
+CREATE VIEW base_result AS
+SELECT
+    *
+FROM
+    sales
+WHERE
+    volume > 1000;
 
-INSERT INTO top_sales (...) -- Can be used to insert data on subquery
+SELECT
+    customer_name,
+    product_name
+FROM
+    base_result;
+
+INSERT INTO
+    top_sales (...) -- Can be used to insert data on subquery
 SELECT
